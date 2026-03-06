4b57c58852b62fdfcc716749a7ac1c0b5767e4be
Ark <discord.ark@outlook.com>
2026-03-06 19:56:02 +0000

    Updating theming

--- src/SplashLogo.tsx
+++ src/SplashLogo.tsx
@@ -4,9 +4,13 @@ import {Logotype} from '#/view/icons/Logotype'
 
 export function SplashLogo() {
   return (
-    <View style={styles.container}>
-      <Logotype fill="#fff" width={100} />
-    </View>
+    <div style={flatten([a.relative, {width: size, height: size * ratio, top: -50}])}>
+      <img
+        src="/assets/icons/logomark.svg"
+        alt="Protosky"
+        style={{width: '100%', height: '100%'}}
+      />
+    </div>
   )
 }
 
